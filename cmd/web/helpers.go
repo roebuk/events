5af81c1232dbda8ec4ca0beb5642d9885c120346
Kristian Roebuck <roebuck86@gmail.com>
2026-01-13 17:51:01 +0000

    Remove duplicate methods

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -57,11 +57,3 @@ func (app *application) clientError(w http.ResponseWriter, status int) {
 func (app *application) notFound(w http.ResponseWriter) {
 	app.clientError(w, http.StatusNotFound)
 }
-
-func (app *application) clientError(w http.ResponseWriter, status int) {
-	http.Error(w, http.StatusText(status), status)
-}
-
-func (app *application) notFound(w http.ResponseWriter) {
-	app.clientError(w, http.StatusNotFound)
-}
